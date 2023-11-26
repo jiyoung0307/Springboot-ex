37e959c11e00d9c8c85c5d800cfe3ddc2b4ae63e
zy_zero <jiyoungjang0307@gmail.com>
2023-11-27 01:01:20 +0900

    4장 롬복과 리팩터ë§

--- src/main/java/com/example/firstproject/dto/ArticleFormDTO.java
+++ src/main/java/com/example/firstproject/dto/ArticleFormDTO.java
@@ -1,42 +1,18 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class ArticleFormDTO {
     private String title;
     private String content;
-
-//    전송받은 제목과 내용을 필드에 저장하는 생성자 추가
-    public ArticleFormDTO() {}
-    public ArticleFormDTO(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    //    데이터를 잘 받았는지 확인할 toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleFormDTO{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
 //        DTO 객체를 엔티티로 변환
