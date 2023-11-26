5df9db993ce9a153ae7c7f1577f9819a63b60b83
zy_zero <jiyoungjang0307@gmail.com>
2023-11-27 01:11:00 +0900

    4.2.4 컨트롤러에 로그 남기기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleFormDTO;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired  // 스프링 부트가 미리 생성해 놓은 리파지토리 객체 주입(DI)
@@ -20,13 +22,16 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleFormDTO formDTO) {                    /** 폼 데이터를 DTO로 받기 */
-        System.out.println("formDTO.toString() ==> " + formDTO.toString());  /** DTO에 폼 데이터가 잘 담겼는지 확인*/
+//        System.out.println("formDTO.toString() ==> " + formDTO.toString());  /** DTO에 폼 데이터가 잘 담겼는지 확인*/
+        log.info(formDTO.toString());
 //         1. DTO를 엔티티로 변환
         Article article = formDTO.toEntity();
-        System.out.println("DTO가 엔티티로 잘 변환되는지 확인 ==> " + article.toString());
+//        System.out.println("DTO가 엔티티로 잘 변환되는지 확인 ==> " + article.toString());
+        log.info(article.toString());
 //         2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println("article이 DB에 잘 저장되는지 확인 ==> " + saved.toString());
+//        System.out.println("article이 DB에 잘 저장되는지 확인 ==> " + saved.toString());
+        log.info(saved.toString());
         return "";
     }
 
