8c0eacbbbcf725781bf301126143592c9cd6cc52
zy_zero <jiyoungjang0307@gmail.com>
2023-11-27 02:10:22 +0900

    5장 모든 데이í 가져오기 형변환까지

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -45,6 +46,15 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
 //        3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index() {
+//        1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+//        2. 가져온 Article 묶음을 모델에 등록하기
+//        3. 사용자에게 보여 줄 뷰 페이지 설정하기
+        return "";
     }
 
 }
