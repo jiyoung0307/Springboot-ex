dfca571bc65bbfa97b3485eca41854737e6164a7
zy_zero <jiyoungjang0307@gmail.com>
2023-11-27 01:05:18 +0900

    4.2.3 엔티티 리팩터링

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,41 +4,27 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-/** 1. 엔티티 선언 */
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 public class Article {
     @Id
-    /** 2. 엔티티의 대푯값 지정 */
+    /** 엔티티의 대푯값 지정 */
     @GeneratedValue
-    /** 3.자동 생성 기능 추가(숫자가 자동으로 매겨짐) */
+    /** 자동 생성 기능 추가(숫자가 자동으로 매겨짐) */
     private Long id;
 
     @Column
-    /** 2. title 필드 선언, DB 테이블의 title 열과 연결됨 */
+    /** title 필드 선언, DB 테이블의 title 열과 연결됨 */
     private String title;
 
     @Column
-    /** 2. content 필드 선언, DB 테이블의 content 열과 연결됨  */
+    /** content 필드 선언, DB 테이블의 content 열과 연결됨  */
     private String content;
 
-//    Article 생성자 추가
-//    Article 객체의 생성 및 초기화를 위해
-    public Article() {}
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    //    toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
